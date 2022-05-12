e7aadd826b73b3c0478c5d2717812a34d2ba8f83
MalakOdtalla <malakodt35@gmail.com>
2022-05-12 08:23:11 +0300

    add class16 branch

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,32 @@
+import Header from '/home/malak/project/Travel-Destination/travel/src/Components/header/Header'
+import Footer from "/home/malak/project/Travel-Destination/travel/src/Components/Footer/Footer";
+import Tours from "/home/malak/project/Travel-Destination/travel/src/Components/Tours/Tours"
+const Data=require('/home/malak/project/Travel-Destination/travel/src/data/db.json')
+
+
+
+function Home(){
+return(
+<>
+<Header/>
+{Data.map(element =>{
+    return(
+<Tours    name={element.name}  img={element.image}/>
+    )
+})
+
+
+}
+
+
+
+<Footer/>
+
+</>
+
+
+);
+
+}
+
+export default Home;
