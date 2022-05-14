28d36800f61f35820e9f72c9002192ebc7e1ca29
MalakOdtalla <malakodt35@gmail.com>
2022-05-14 22:59:43 +0300

    add routs

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,10 @@
-import Header from '/home/malak/project/Travel-Destination/travel/src/Components/header/Header'
-import Footer from "/home/malak/project/Travel-Destination/travel/src/Components/Footer/Footer";
-import Tours from "/home/malak/project/Travel-Destination/travel/src/Components/Tours/Tours"
+import Header from '../header/Header'
+import Footer from "../Footer/Footer";
+import Tours from "../Tours/Tours"
 
 
 
-const Data=require('/home/malak/project/Travel-Destination/travel/src/data/db.json')
+const Data=require('../../data/db.json')
 
 
 
